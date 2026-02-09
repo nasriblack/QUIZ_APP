65e1f255ac739a122ed4a078bbf799edf502a466
Nasereddine Lakhal <naserlakhal@gmail.com>
2026-02-09 21:42:13 +0100

    question package

---
+++ quizapp-mono/src/test/java/com/blacode/quizapp/QuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blacode.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
